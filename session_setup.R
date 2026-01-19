72281e08744ffe91d3e1d12f8f6f5be26cb0d053
Harry Green <36624710+hdg204@users.noreply.github.com>
2026-01-19 11:00:25 +0000

    Adding read_death
    I forgot this was a thing

--- session_setup.R
+++ session_setup.R
@@ -153,11 +153,29 @@ read_SR=function(SR_codes){
 	return(sr2)
 }
 
+read_death <- function(codes){
+  # The below block of code produces a grep command  which is used to create temp.tsv. 
+  grep_codes(codes,'death_cause.tsv')
+  # This awk runs a join at system level matching on the record id to create joined.tsv (but only includes the record column not the ICD10). This command was written with AI assistance 
+  system("awk -F'\t' 'NR==FNR {keys[$2]; next} FNR==1 || $1 in keys' temp.tsv death.tsv > joined.tsv")
+  diag_data=read.csv("temp.tsv",sep='\t')
+  record_data=read.csv("joined.tsv",sep='\t')
+  all_data=inner_join(diag_data,record_data,by = c('dnx_death_id', 'eid', 'ins_index'))
+  all_data=all_data%>%select('eid','ins_index','arr_index','level','cause_icd10','date_of_death')
+  # Just in case, do a final pass to make sure that the ICD10 code came up in the diag_icd10 column
+  all_data=all_data[df_grep(codes,all_data$cause_icd10),]
+  all_data$date_of_death=as.Date(all_data$date_of_death)
+  system('rm joined.tsv')
+  return(baseline_combo(all_data,'date_of_death'))
+}
+
+
 source('https://raw.githubusercontent.com/ExeterGenetics/ukbextractR/main/baseline_table.R')
 
 print('Thank you for using ukbextractR Version 1.0, by Harry Green and Jiaqi Li, and ukbrapR by Luke Pilling, University of Exeter')
 
 print('For any issues, please contact Harry Green at h.d.green@exeter.ac.uk')
+
 
 
 
